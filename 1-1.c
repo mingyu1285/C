5665a6547ee3e1e63ac5f5cd74484c78bda288c4
mingyu1285 <56614072+mingyu1285@users.noreply.github.com>
2023-04-11 09:33:45 +0900

    Add files via upload

---
+++ 1-1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void main(){
+	int a, b;
+	int result;
+
+	printf("첫 번째 계산할 값을 입력하세요:");
+	scanf("%d", &a);
+
+	printf("두 번째 계산할 값을 입력하세요:");
+	scanf("%d",&b);
+
+	printf("%d+%d=%d",a,b,result=a+b);
+	printf("\n");
+
+	printf("세 번째 계산할 값을 입력하세요:");
+	scanf("%d", &a);
+
+	printf("네 번째 계산할 값을 입력하세요:");
+	scanf("%d",& b);
+
+	printf("%d*%d=%d",a,b, result=a*b);
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+}
